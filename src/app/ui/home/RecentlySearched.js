16de8fd0fb35f34e6b36f0023725e88f5f309dbf
wzeddie <89380062@qq.com>
2024-06-30 13:16:37 +0800

    1

--- src/app/ui/home/RecentlySearched.js
+++ src/app/ui/home/RecentlySearched.js
@@ -7,7 +7,7 @@ export default function RecentlySearched({ recentDomains }) { // 组件接收 pr
   const [isClickable, setIsClickable] = useState(true); // 控制链接可点击状态
   const RSformRef = useRef(null); // 使用 useRef 来引用表单，在 form 中，将表单的引用赋值给 formRef
   const router = useRouter();
-
+//获取点击事件后，直接发起fech api请求
   const onClick_a = async (event, domain) => {
       event.preventDefault(); // 阻止默认的表单提交行为
       setIsClickable(false); // 设置链接为不可点击状态
@@ -27,28 +27,10 @@ export default function RecentlySearched({ recentDomains }) { // 组件接收 pr
     }
     
   };
-  const RShandleSubmit  = async (event) => {
-    event.preventDefault();
-    const form = event.target;//获取表单元素
-    const formData = new FormData(form);
-    // 将 FormData 转换为 application/x-www-form-urlencoded 格式的字符串
-    const data = Object.fromEntries(formData.entries());
-    console.log(data)
-    const response = await fetch('/api/submit-form-2', {//对应后台api接口
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',//设置为服务端可以方便获取的接收格式
-      },
-      body: JSON.stringify(data),
-    });
-    if (response.redirected) {
-      router.push(response.url);//接收服务器重定向
-    }
-  }
-
+  
   return (
     <div className="flex items-center justify-between py-4">
-        <form ref={RSformRef} id="myRSForm" onSubmit={RShandleSubmit} >
+        <form ref={RSformRef} id="myRSForm" >
           <input type="hidden" name="user_domain" /> {/* 隐藏输入框，用于存储被点击的域名 */}
           </form>
 
