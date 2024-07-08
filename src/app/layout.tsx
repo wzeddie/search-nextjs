cf80377d58b8251fcdca61c2dce055b1dabd09b2
wzeddie <89380062@qq.com>
2024-07-08 23:29:51 +0800

    添加广告脚本在head中

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/global.css';//网页CSS
 import { Metadata } from 'next';//网页元数据
 import Head from 'next/head';
+import Script from "next/script"
 //import { inter } from '@/app/ui/fonts'; 导入google字体
 import Header from './ui/header.js';//页眉
 import Footer from './ui/footer.js';//页脚
@@ -14,6 +15,7 @@ interface ExtendedMetadata extends Metadata {
   metadataBase: URL;
   googleAdsenseAccount: string; // 新增属性
   keywords: string;
+  // scripts:string[];
 }
 export const metadata: ExtendedMetadata = {
   title: {
@@ -22,9 +24,15 @@ export const metadata: ExtendedMetadata = {
   },
   description: 'Discover the perfect domain name for your website with Lean Domain Search. Our intuitive platform helps you find short, memorable, and available domain names quickly and easily. Start your online journey today!',
   metadataBase: new URL('http://lean-domain.online'),
-  googleAdsenseAccount:'ca-pub-7801648526886850',
-  keywords:'Lean Domain Search, domain name generator, find domain names, unique domain names, domain search tool, domain availability, domain suggestions, domain brainstorming, domain name ideas, domain name search'
-
+  googleAdsenseAccount: 'ca-pub-7801648526886850',
+  keywords: 'Lean Domain Search, domain name generator, find domain names, unique domain names, domain search tool, domain availability, domain suggestions, domain brainstorming, domain name ideas, domain name search',
+  // scripts: [
+  //   Script({
+  //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7801648526886850',
+  //     async: true,
+  //     crossOrigin: 'anonymous',
+  //   }),
+  // ],
 };
 export default function RootLayout({
   children,
@@ -33,21 +41,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <Head>
-        {/* 其他<head>中的元素 */}
+      {/* <meta name="google-adsense-account" content="ca-pub-7801648526886850"> */}
+
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7801648526886850"
           crossOrigin="anonymous"
         ></script>
-      </Head>
-      <body>
-        <main>
-          <Header />
-          {children}
-          <Footer />
-        </main>
-      </body>
+
+        <body>
+          <main>
+            <Header />
+            {children}
+            <Footer />
+          </main>
+        </body>
     </html>
   )
 }
